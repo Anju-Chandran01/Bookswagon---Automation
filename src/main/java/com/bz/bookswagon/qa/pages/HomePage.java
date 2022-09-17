c7803a39e735d27cd710b081c2034022cd35cdc4
Anju-Chandran01 <anjuchandrancs01@gmail.com>
2022-09-17 08:10:06 +0530

    Added : Home page Test

--- src/main/java/com/bz/bookswagon/qa/pages/HomePage.java
+++ src/main/java/com/bz/bookswagon/qa/pages/HomePage.java
@@ -1,6 +1,61 @@
 package com.bz.bookswagon.qa.pages;
 
 import com.bz.bookswagon.qa.base.TestBase;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
 
 public class HomePage extends TestBase {
+
+    // pagefactory OR object repository
+
+    @FindBy(id="inputbar")
+    WebElement searchBar;
+
+    @FindBy(id="btnTopSearch")
+    WebElement searchButton;
+
+    @FindBy(xpath="//h1[contains(text(),'My Account')]")
+    WebElement pageHeading;
+
+    @FindBy(xpath="//a[normalize-space()='My Account']")
+    WebElement myAccount;
+
+    @FindBy(xpath="//a[normalize-space()='Personal Settings']")
+    WebElement personalSettings ;
+
+    @FindBy(xpath="//a[normalize-space()='My Orders']")
+    WebElement myOrders;
+
+    @FindBy(xpath = "//a[normalize-space()='My Wishlist")
+    WebElement myWishlist;
+
+    @FindBy(xpath="//a[normalize-space()='My Gift Certificates']")
+    WebElement myGiftCertificates;
+
+    @FindBy(xpath="//a[normalize-space()='My Addresses']")
+    WebElement myAddresses;
+
+    @FindBy(xpath="//a[normalize-space()='Change Password']")
+    WebElement changePassword;
+
+    //initialising page objects
+    public HomePage() {
+        PageFactory.initElements(driver, this);
+    }
+
+    //actions
+    public String validate_HomePage_Url(){
+        return driver.getCurrentUrl();
+    }
+
+    public boolean validate_HomePage_Heading(){
+        return pageHeading.isDisplayed();
+    }
+
+    public SearchBooksPage searchBooksUsingSearchBar(String bookName){
+        searchBar.sendKeys(bookName);
+        searchButton.click();
+        return new SearchBooksPage();
+    }
 }
--- src/test/java/com/bz/bookswagon/qa/testcases/HomePageTest.java
+++ src/test/java/com/bz/bookswagon/qa/testcases/HomePageTest.java
@@ -1,6 +1,50 @@
 package com.bz.bookswagon.qa.testcases;
 
 import com.bz.bookswagon.qa.base.TestBase;
+import com.bz.bookswagon.qa.pages.HomePage;
+import com.bz.bookswagon.qa.pages.LoginPage;
+import com.bz.bookswagon.qa.pages.SearchBooksPage;
+import org.testng.Assert;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
 
 public class HomePageTest extends TestBase {
+
+    LoginPage loginPage;
+    HomePage homePage;
+    SearchBooksPage searchBooksPage;
+
+    public HomePageTest(){
+        super();
+    }
+
+    @BeforeMethod
+    public void setUp(){
+        initialization();
+        loginPage = new LoginPage();
+        homePage = loginPage.login(prop.getProperty("mobile"), prop.getProperty("pass"));
+    }
+
+    @Test(priority = 1)
+    public void homePage_Heading_Test(){
+        boolean flag = homePage.validate_HomePage_Heading();
+        Assert.assertTrue(flag);
+    }
+
+    @Test(priority = 2)
+    public void homePage_UrlTest(){
+        String url = homePage.validate_HomePage_Url();
+        Assert.assertEquals(url,"https://www.bookswagon.com/myaccount.aspx");
+    }
+
+    @Test(priority = 3)
+    public void searchBoxTest(){
+        searchBooksPage = homePage.searchBooksUsingSearchBar("Wings of Fire");
+    }
+
+    @AfterMethod
+    public void tearDown(){
+        driver.quit();
+    }
 }
